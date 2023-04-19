97742804c4c129c761f253eb727df058200baafe
TiggerYuan <775142365@qq.com>
2023-04-19 21:27:15 +0800

    add project

---
+++ rear end/LargeDataTablesTestApi/LargeDataTablesTestApi/Program.cs
@@ -0,0 +1,71 @@
+using LargeDataTablesTestApi;
+
+var builder = WebApplication.CreateBuilder(args);
+const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:5173/", "http://www.contoso.com")
+                                                  .AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                      });
+});
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseCors();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/api/tables", () =>
+{
+    List<TableData> data = new List<TableData>();
+    for (int i = 0; i < 5000; i++)
+    {
+        data.Add(new TableData()
+        {
+            Id=i,
+            Name=$"Nmae {i.ToString()}"
+        });
+    }
+    return data;
+})
+.WithName("GetTables")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ rear end/LargeDataTablesTestApi/LargeDataTablesTestApi/TableData.cs
@@ -0,0 +1,9 @@
+﻿namespace LargeDataTablesTestApi
+{
+    public class TableData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Index { get; set; }
+    }
+}
